eb57e96a2ca03055eddca0e7629ac75770fa7f8a
Saba <nigistsaba@hotmail.com>
2017-10-14 16:01:53 +0200

    adds class for starshower result and enum for rating

---
+++ src/main/java/com/shootingstars/shootingstars/models/Rating.java
@@ -0,0 +1,25 @@
+package com.shootingstars.shootingstars.models;
+
+public enum Rating {
+
+    STARK("Stark"), MITTEL("Mittel"), SCHWACH("Schwach"), IRREGULAR("Irregular");
+
+    String stringValue;
+
+    Rating(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    String getStringValue() {
+        return this.stringValue;
+    }
+
+    public static Rating fromString(String stringValue) {
+        for (Rating rating: Rating.values()) {
+            if (rating.stringValue.equalsIgnoreCase(stringValue)) {
+                return rating;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/shootingstars/shootingstars/models/StarShowerResult.java
@@ -0,0 +1,14 @@
+package com.shootingstars.shootingstars.models;
+
+import java.time.LocalDate;
+
+public class StarShowerResult {
+
+    String name;
+    LocalDate startDate;
+    LocalDate endDate;
+    LocalDate peakDate;
+    int declination;
+    Rating rating;
+
+}
