02bcdbe80c1d85a78cc90df2505cb8989b1fecb7
mignonnesaurus <elifloresch@gmail.com>
2017-10-14 12:57:39 +0200

    Initial setup of spring boot

---
+++ src/main/java/com/shootingstars/shootingstars/ShootingStarsApplication.java
@@ -0,0 +1,12 @@
+package com.shootingstars.shootingstars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootingStarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShootingStarsApplication.class, args);
+	}
+}
