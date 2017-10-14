29eed59935f302b5215b752b64555169a04b80e2
mignonnesaurus <elifloresch@gmail.com>
2017-10-14 16:36:11 +0200

    Create the test

---
+++ src/test/java/com/shootingstars/services/CsvReaderTest.java
@@ -0,0 +1,12 @@
+package com.shootingstars.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CsvReaderTest {
+    @Test
+    public void parse() throws Exception {
+    }
+
+}
